ff9ee99a26b97bb43a42fad3d4849f88963ad8d4
MSinghSolanki <112701441+MSinghSolanki@users.noreply.github.com>
2023-06-17 16:05:20 +0530

    new

--- src/api.js
+++ src/api.js
@@ -8,4 +8,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "360e96144e8eb0fca116c4be6816e37e"; // enter your key from openweather API
